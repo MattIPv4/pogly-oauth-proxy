13d61e4478632813a5005d876b70fd05600a5de1
James Williams <jr_williams@me.com>
2025-03-17 15:15:50 +0000

    Update src/database.ts
    Co-authored-by: Matt Cowley <me@mattcowley.co.uk>

--- src/database.ts
+++ src/database.ts
@@ -42,17 +42,9 @@ const isDatabase = (value: any): value is Database => {
   ]);
 };
 
-const getDatabasePath = () => {
-  if (process.env.DATA_PATH) {
-    const path = new URL("data.json", process.env.DATA_PATH);
-    return path;
-  }
-
-  const path = new URL("../data.json", import.meta.url);
-  return path;
-};
-
-const path = getDatabasePath();
+const path = process.env.DATA_PATH
+  ? new URL("data.json", process.env.DATA_PATH)
+  : new URL("../data.json", import.meta.url);
 
 const load = async (): Promise<Database> => {
   const data = await readFile(path, "utf8");
